9f9c15dc31fa29c48c2c416c48c4780a49a16ffd
Lê Hoàng Sơn <sonlearnit8386@gmail.com>
2026-04-07 01:40:35 +0700

    Feat/setup (#5)
    * thêm file .gitignore
    
    * feat(backend): khởi tạo project Django và thiết kế Database Models
    
    * feat(frontend): khỏi tạo giao diện chat ban đầu
    
    * thêm cache vào gitignore
    
    * nâng cấp thêm gitignore
    
    * feat(upload): xây dựng giao diện và chức năng tải file lên database
    
    * feat(ai): tích hợp LangChain để đọc và băm nhỏ tài liệu (Text Extraction & Chunking)
    
    ---------
    
    Signed-off-by: Lê Hoàng Sơn <sonlearnit8386@gmail.com>

--- rag/views.py
+++ rag/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .models import Document
+from .utils import process_document
 
-# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES.get('document')
+
+        if uploaded_file:
+            file_extension = 'pdf' if uploaded_file.name.lower().endswith('.pdf') else 'docx'
+
+            doc = Document.objects.create(
+                file=uploaded_file,
+                file_type=file_extension
+            )
+
+            #Chunkin' ở chỗ này
+            chunks = process_document(doc.file.path, doc.file_type)
+
+            if chunks:
+                messages.success(request, f'tải thành công: {doc.filename}. Hệ thống chia thành {len(chunks)} đoạn dữ liệu')
+            else:
+                messages.warning(request, f'Đã tải file nhưng không tìm thấy dữ liệu')
+
+            messages.success(request, f'Đã tải lên và lưu thành công: {doc.filename}')
+        else: 
+            messages.error(request, 'file tải lên chỉ hỗ trợ PDF và DOCX')
+    return render(request, 'rag/index.html')
