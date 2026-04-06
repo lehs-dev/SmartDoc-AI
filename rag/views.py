8b2ae6a09800e4e25604e593d9a21c23df7070ff
lehs-dev <sonlearnit8386@gmail.com>
2026-04-06 23:04:57 +0700

    feat(upload): xây dựng giao diện và chức năng tải file lên database

--- rag/views.py
+++ rag/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .models import Document
 
 def index(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES.get('document')
+
+        if uploaded_file:
+            file_extension = 'pdf' if uploaded_file.name.lower().endswith('.pdf') else 'docx'
+
+            doc = Document.objects.create(
+                file=uploaded_file,
+                file_type=file_extension
+            )
+
+            messages.success(request, f'Đã tải lên và lưu thành công: {doc.filename}')
+        else: 
+            messages.error(request, 'file tải lên chỉ hỗ trợ PDF và DOCX')
     return render(request, 'rag/index.html')
