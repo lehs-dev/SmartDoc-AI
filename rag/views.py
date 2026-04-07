b88b20ba0012402dc3ede320656c9e7374bc89f0
lehs-dev <sonlearnit8386@gmail.com>
2026-04-07 12:28:00 +0700

    thêm lịch sử chat

--- rag/views.py
+++ rag/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.contrib import messages
-from .models import Document
+from .models import Document, ChatSession, ChatMessage
 from .utils import process_document, get_vector_store, ask_gemma
 import json
 from django.http import JsonResponse
 
 def index(request):
+    sessions = ChatSession.objects.all().order_by('-created_at')
+
+    current_session_id = request.GET.get('session')
+    current_messages = []
+
+    if current_session_id:
+        # Lấy toàn bộ tin nhắn của session đó
+        current_messages = ChatMessage.objects.filter(session_id=current_session_id).order_by('created_at')
+
     if request.method == 'POST':
         uploaded_file = request.FILES.get('document')
 
@@ -31,6 +40,12 @@ def index(request):
             messages.success(request, f'Đã tải lên và lưu thành công: {doc.filename}')
         else: 
             messages.error(request, 'file tải lên chỉ hỗ trợ PDF và DOCX')
+
+    context = {
+        'sessions' : sessions,
+        'current_session_id' : current_session_id,
+        'current_messages' : current_messages,
+    }
     return render(request, 'rag/index.html')
 
 def chat_api(request):
@@ -39,13 +54,23 @@ def chat_api(request):
         try:
             data = json.loads(request.body)
             user_question = data.get('message')
+            session_id = data.get('session_id')
 
             if not user_question:
                 return JsonResponse({'error' : 'phải nhập câu hỏi'}, status=400)
             
+            if session_id:
+                session = ChatSession.objects.get(id=session_id)
+            else:
+                title = user_question[:30] + "..." if len(user_question) > 30 else user_question
+                session = ChatSession.objects.create(title=title)
+            
+            ChatMessage.objects.create(session=session, role='user', content=user_question)
+            
             bot_answer = ask_gemma(user_question)
 
-            return JsonResponse({'response' : bot_answer})
+            return JsonResponse({'response' : bot_answer,
+                                 'session_id' : session.id})
         
         except Exception as e :
             return JsonResponse({'error' : str(e)}, status=500)
